827481556846eb092693726305a9b532b4c60524
Anthony <anthonymatheusds@gmail.com>
2026-04-29 12:16:20 -0300

    first commit

---
+++ scrapLaw.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+import time
+
+st.title('Scrapping de Leis Municipais')
+
+link=st.text_input('Digite o link da lei que deseja scrappear')
+
+if link is not None:
+    options = Options()
+    options.add_argument('--headless')
+    nav=webdriver.Chrome(options=options)
+    time.sleep(0.5)
+    nav.get(link)
+    time.sleep(0.5)
+    l=nav.find_elements(By.CLASS_NAME,"tooltips")
+    leis=[]
+    for i in l:
+        leis.append(i.text)
+    xpath='//*[@id="conteudo_diploma"]/div/div[1]/center/font'
+    texto=nav.find_element(By.XPATH,xpath).text
+    df=pd.DataFrame({'Lei':[texto]*len(leis),'Conecta':leis})
+    st.dataframe(df)
+    nav.quit()
